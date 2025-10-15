e7df531ba498fda801bf140b90163ed3de783967
Pomeron <oleksa.b@gmail.com>
2025-10-15 16:26:01 -0400

    Updated version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scs-kc",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     include_package_data=True,  # include files specified in MANIFEST.in
     description="Package for Kerr-cat optimization with supercoefficients",
